c5fdc5431b8e973b0adc03aad1e1b9190a87c546
Hedrick, Brandon L <bhedrick.purdue@gmail.com>
2018-04-16 17:33:21 -0500

    remove test code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css';
-import { Button, Icon } from 'semantic-ui-react';
 
 class App extends Component {
   render() {
@@ -10,10 +9,6 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">My Connections</h1>
         </header>
-        <Button primary icon labelPosition='right'>
-          Edit
-          <Icon name='edit' />
-        </Button>
       </div>
     );
   }
